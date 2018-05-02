b151200e235b3af8eb634734e992b6fcec58bc81
faouellet <faouellet@gmail.com>
2018-05-01 20:40:53 -0400

    Adding utility function to find set bit position

--- core/utils.h
+++ core/utils.h
@@ -16,3 +16,20 @@ constexpr unsigned int GetNbSetBits()
 
     return count;
 }
+
+template <uint64_t TVal>
+constexpr unsigned int GetSetBitPosition()
+{
+    static_assert(TVal != 0, "No bit set in 0");
+    static_assert(TVal & (TVal - 1) == 0, "More than one bit set");
+
+    unsigned int pos{};
+    uint64_t val = TVal;
+    while(!(val & 1))
+    {
+        ++pos;
+        val >>= 1;
+    }
+
+    return pos;
+}
